25a4367f10bb8e6aab530d7c4426921fa1bfb2b8
kimwoodfield <kimwoodfield@live.com>
2025-07-02 11:25:47 +1000

    Add conditional for firefox

--- modules/tinymce/src/core/test/ts/browser/content/EditorContentTest.ts
+++ modules/tinymce/src/core/test/ts/browser/content/EditorContentTest.ts
@@ -5,6 +5,7 @@ import { TinyApis, TinyAssertions, TinyHooks } from '@ephox/wrap-mcagar';
 import { assert } from 'chai';
 
 import Editor from 'tinymce/core/api/Editor';
+import Env from 'tinymce/core/api/Env';
 import { BeforeGetContentEvent, BeforeSetContentEvent, GetContentEvent, SetContentEvent } from 'tinymce/core/api/EventTypes';
 import Entities from 'tinymce/core/api/html/Entities';
 import AstNode from 'tinymce/core/api/html/Node';
@@ -288,7 +289,9 @@ describe('browser.tinymce.core.content.EditorContentTest', () => {
 
         it('TINY-10088: Preserve attributes with self closed HTML tag', () => {
           const rawValue = 'title=<br/>';
-          const content = `<div data-some-attribute="${Entities.encodeRaw(rawValue, true)}">abc</div>`;
+          const content = Env.browser.isFirefox() ?
+            `<div data-some-attribute="${rawValue}">abc</div>` :
+            `<div data-some-attribute="${Entities.encodeRaw(rawValue, true)}">abc</div>`;
           const editor = hook.editor();
           editor.setContent(content);
           TinyAssertions.assertContent(editor, content, { format: 'raw' });
